3429868b84768130de652653cd63d1a566e104a6
ygnaiih1680 <48740869+ygnaiih1680@users.noreply.github.com>
2019-11-13 11:40:30 +0900

    1113

--- src/os/Main.java
+++ src/os/Main.java
@@ -3,6 +3,8 @@ package os;
 public class Main {
 
 	public static void main(String[] args) {
+		/*exe 코드는 cpu instruction 으로 나뉘어진다.
+		 * */
 		OperatingSystem operatingSystem = new OperatingSystem();
 		operatingSystem.run();
 	}
--- src/os/OperatingSystem.java
+++ src/os/OperatingSystem.java
@@ -2,11 +2,20 @@ package os;
 
 public class OperatingSystem {
 	private UXManager uxManager;
-	private Loader loader;
+	private Loader loader;//프로세스매니저나 ux매니저로 들어가도 상관없음.
+	/*io매니저는 간단한 경우 하나만 있을 수도 여러 개 있을 수도 있다.
+	메모리 컨트롤러, 디바이스 컨트롤러-작은 cpu
+	디바이스 인터럽트 발생은 독립적인 링크를 통해 스테이터스 레지스터에 전달한다.
+	time expired, io start, io finish, halt
+	소프트웨어 인터럽트, 하드웨어 인터럽트
+	cisc, risc
+	조각모음-compaction
+	*/
+
 	private FileManager fileManager;
 	private MemoryManager memoryManager;
 	private ProcessManager processManager;
-	
+
 	public OperatingSystem() {
 		this.uxManager = new UXManager();
 		this.loader = new Loader();
